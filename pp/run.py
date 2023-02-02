b46d9eca23aaac95cc7c7d666d612ff3dc17c7f8
My2cola <124276837+My2cola@users.noreply.github.com>
2023-02-02 20:34:33 +0300

    добавлены команды для упрощения взаимодействия с консолью

--- pp/run.py
+++ pp/run.py
@@ -1,5 +1,34 @@
-from epicUpdates import amongus, FallGuys, fortnitePars, GenshinImpact, warface
-from batltleNet import Hearthstone
-from lesta import wordOfTanks
-from RiotUpdates import lolUpdates, parsVal
-from steam import apex, warzone2, bot
+def process_command(command):
+    if command == "/epic":
+        from epicUpdates import amongus, FallGuys, fortnitePars, GenshinImpact, warface
+        print("Welcome to the Epic Games Store!")
+        # ...
+    elif command == "/steam":
+        from steam import apex, warzone2, bot
+        print("Welcome to Steam!")
+        # ...
+    elif command == "/riot":
+        from RiotUpdates import lolUpdates, parsVal
+        print("Welcome to Riot Games!")
+        # ...
+    elif command == "/battleNet":
+        from batltleNet import Hearthstone
+        print("Welcome to Battle.net!")
+        # ...
+    elif command == "/lesta":
+        from lesta import wordOfTanks
+        print("Welcome to Lesta Studios!")
+        # ...
+    elif command == "/help":
+        # code to handle "/help" command
+        print("Available commands: /epic, /steam, /riot, /battleNet, /lesta, /help")
+        # ...
+    else:
+        # code to handle unknown commands
+        print("Unknown command. Type /help for available commands.")
+        # ...
+
+while True:
+    user_input = input("Enter a command: ")
+    process_command(user_input)
+
